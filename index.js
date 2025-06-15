f0aae6609b37b5cf40276f622493e3ad6b4508e3
grodzila <149613916+grodzila@users.noreply.github.com>
2025-06-15 14:59:49 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// This is a simple calculator implementation in JavaScript.
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  display.value = eval(display.value);
+}
